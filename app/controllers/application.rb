2a8841979b61887d13eb9ff65b95d34b0ad9103f
nrdpfc <nrdpfc@d967892f-8c18-4166-bd36-c046b3cbcffb>
2007-11-15 03:36:02 +0000

    git-svn-id: https://svn.nrdpfc.info/repos/nrdpfc/trunk@5 d967892f-8c18-4166-bd36-c046b3cbcffb

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,6 +1,6 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-
+# test new respository - jim
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_nrdpfc_session_id'
