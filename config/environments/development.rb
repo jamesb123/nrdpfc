480cca37d9fc73280afa9d669d225ae56aa3f015
tim <tim@d967892f-8c18-4166-bd36-c046b3cbcffb>
2007-11-24 04:28:49 +0000

    cleaned up examples database.yml's implemented a my-sandbox-only custom override so I can have a custom database.yml file
    git-svn-id: https://svn.nrdpfc.info/repos/nrdpfc/trunk@22 d967892f-8c18-4166-bd36-c046b3cbcffb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,3 +16,8 @@ config.action_view.debug_rjs                         = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+sandbox_config = File.join(File.dirname(__FILE__), "development.sandbox.rb")
+if File.exists?(sandbox_config)
+  eval File.read(sandbox_config), binding, sandbox_config, 1
+end
