6d7ba3901aef3a269d5423c4cb4404165e7d0953
tim <tim@d967892f-8c18-4166-bd36-c046b3cbcffb>
2007-11-24 07:19:38 +0000

    bugfix - method sanitizer handles symbols and strings now
    git-svn-id: https://svn.nrdpfc.info/repos/nrdpfc/trunk@31 d967892f-8c18-4166-bd36-c046b3cbcffb

--- test/unit/evaluate_attribute_method_test.rb
+++ test/unit/evaluate_attribute_method_test.rb
@@ -1,10 +1,26 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class EvaluateAttributeMethodTest < Test::Unit::TestCase
+  def setup
+    @model = Class.new(MicrosatelliteFinalHorizontal)
+  end
+    
   def test_should_sanitize
-    method_name = "This method-stinks"
-#    dbg
-    MicrosatelliteFinalHorizontal.send(:evaluate_attribute_method, method_name, "def #{method_name}; true; end;")
-    assert MicrosatelliteFinalHorizontal.new.respond_to?("This_method_stinks")
+    define_method("This method-stinks")
+    assert_responds_to("This_method_stinks")
+  end
+  
+  def test_should_handle_symbols
+    define_method(:hello)
+    assert_responds_to("hello")
+  end
+protected
+  def define_method(method_name)
+    @model.send(:evaluate_attribute_method, method_name, "def #{method_name}; true; end;")
+  end
+  
+  def assert_responds_to(method_name)
+    assert @model.new.respond_to?(method_name)
+    
   end
 end
