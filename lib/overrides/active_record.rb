3a104b82415890ed5731c03beaf88a0be218da9d
tim <tim@d967892f-8c18-4166-bd36-c046b3cbcffb>
2007-11-24 07:17:16 +0000

    attr_name.to_s
    git-svn-id: https://svn.nrdpfc.info/repos/nrdpfc/trunk@30 d967892f-8c18-4166-bd36-c046b3cbcffb

--- lib/overrides/active_record.rb
+++ lib/overrides/active_record.rb
@@ -1,8 +1,9 @@
 module ActiveRecord::AttributeMethods::ClassMethods
+  INVALID_CHARS_REGEXP = /[^a-z_0-9\?]/i
   def evaluate_attribute_method_with_safety(attr_name, method_definition, method_name=attr_name)
-    invalid_chars_regexp = /[^a-z_0-9\?]/i
-    if invalid_chars_regexp.match(attr_name)
-      new_attr_name = attr_name.gsub(invalid_chars_regexp, "_")
+    attr_name = attr_name.to_s
+    if INVALID_CHARS_REGEXP.match(attr_name)
+      new_attr_name = attr_name.gsub(INVALID_CHARS_REGEXP, "_")
       method_definition.gsub!("def #{attr_name}", "def #{new_attr_name}")
     end
     
