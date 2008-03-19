1d60ee3ed53bff96dbb558942d290abec79574eb
nrdpfc <nrdpfc@d967892f-8c18-4166-bd36-c046b3cbcffb>
2008-03-19 16:34:42 +0000

    I need to do something to handle the Rails 2.0.1 vs 2.0.2 issue.  This fixes that temporarily.
    git-svn-id: https://svn.nrdpfc.info/repos/nrdpfc/trunk@121 d967892f-8c18-4166-bd36-c046b3cbcffb

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@
 ENV['RAILS_ENV'] ||= ENV["HTTP_RAILS_ENV"]
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.0.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
