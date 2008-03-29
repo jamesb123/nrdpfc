d84bd04354ee9966c182536d7415490410851e1f
tim <tim@d967892f-8c18-4166-bd36-c046b3cbcffb>
2008-03-29 13:21:31 +0000

    updated for db changes
    git-svn-id: https://svn.nrdpfc.info/repos/nrdpfc/trunk@143 d967892f-8c18-4166-bd36-c046b3cbcffb

--- lib/compiler/microsatellite_compiler.rb
+++ lib/compiler/microsatellite_compiler.rb
@@ -32,10 +32,8 @@ class Compiler::MicrosatelliteCompiler < Compiler::MicrosatelliteCompilerBase
       t.integer :organism_index
       
       self.locii.each { |locus|
-        # t.string "#{locus}a"
-        # t.string "#{locus}b"
-        t.string "#{locus}a", :limit => 7
-        t.string "#{locus}b", :limit => 7        
+        t.string "#{locus}a", :float
+        t.string "#{locus}b", :float
       }
     end
     
