0d39122dd52bfdd4b07867091f6a5db2e5f45a79
nrdpfc <nrdpfc@d967892f-8c18-4166-bd36-c046b3cbcffb>
2008-04-10 15:13:50 +0000

    Some user controller stuff
    git-svn-id: https://svn.nrdpfc.info/repos/nrdpfc/trunk@174 d967892f-8c18-4166-bd36-c046b3cbcffb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,22 @@ class UsersController < ApplicationController
   layout "tabs"
   active_scaffold :users do |config|
     config.columns = [:login, :email]
-    config.actions.exclude :create, :delete
+    config.create.columns = [:login, :email, :password, :password_confirmation]
   end
   
+  # These go somewhere else...
+  def password_form_column(record, field_name)
+    password_field_tag field_name, record.password
+  end
+  
+  def password_confirmation_form_column(record, field_name)
+    password_field_tag field_name, record.password_confirmation
+  end
+
 end
 
   #  config.actions = [:create, :update, :show]  
 
     #config.actions.exclude :create
     #config.actions.add :delete
+    
