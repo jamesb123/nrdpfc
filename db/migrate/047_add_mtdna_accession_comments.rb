68259f162ca16defc5b03d603d24250cf6febbfa
(no author) <(no author)@d967892f-8c18-4166-bd36-c046b3cbcffb>
2008-07-01 20:08:03 +0000

    migration fix 2
    git-svn-id: https://svn.nrdpfc.info/repos/nrdpfc/trunk@222 d967892f-8c18-4166-bd36-c046b3cbcffb

--- db/migrate/047_add_mtdna_accession_comments.rb
+++ db/migrate/047_add_mtdna_accession_comments.rb
@@ -1,9 +1,9 @@
 class AddMtdnaAccessionComments < ActiveRecord::Migration
   def self.up
-      add_column :mt_dnas_seqs, :accession, :string, :limit => 30
+      add_column :mt_dna_seqs, :accession, :string, :limit => 30
   end
 
   def self.down
-      remove_column :mt_dnas, :accession
+      remove_column :mt_dna_seqs, :accession
   end
 end
