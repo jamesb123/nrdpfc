04f4c1b8afc8c9c37c35e0a046613e3c36f74659
Tim Harper <timcharper@gmail.com>
2008-08-09 22:36:34 -0600

    cleanup

--- app/controllers/gender_final_horizontals_controller.rb
+++ app/controllers/gender_final_horizontals_controller.rb
@@ -6,9 +6,7 @@ class GenderFinalHorizontalsController < ApplicationController
   end
   
   def custom_reconfiguration(config)
-    config.columns = [:project, :sample]
     config.list.columns = [:project, :organism_code, :raw_data] + @model.dynamic_column_names
-    #config.action_links.add('go_to', :label => "Go To...", :page => true) 
   end
   
 public
