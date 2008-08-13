ee247ea60dbaaabf3ec9f7ba9bf7789678ac0c56
james burrett <james@burrett.org>
2008-08-12 22:22:59 -0400

    sample model change - remove sample id from to_label

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -99,7 +99,7 @@ class Sample < ActiveRecord::Base
   
   def to_label 
     if !organism.nil?  
-      return "#{organism.organism_code} - #{organism_index}: (#{self.id})"
+      return "#{organism.organism_code} - #{organism_index}"
     else
       return "#{self.id} SID" 
     end
