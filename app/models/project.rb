bfe0295ac516c9a4b7f76ed251f3d49ffde996aa
Tim Harper <timcharper@gmail.com>
2008-08-16 22:52:54 -0600

    Added microsatellite_final_horizontals

--- app/models/project.rb
+++ app/models/project.rb
@@ -23,6 +23,7 @@ class Project < ActiveRecord::Base
   has_many :y_chromosome_seqs
   has_many :security_settings
   has_many :microsatellite_horizontals
+  has_many :microsatellite_final_horizontals
   has_many :mt_dna_final_horizontals
   has_many :y_chromosome_final_horizontals
   has_many :mhc_final_horizontals
