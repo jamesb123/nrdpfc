97d365f1b98f57ca391bc35dba5e91226b52e9eb
james burrett <james@burrett.org>
2008-10-16 14:04:55 -0400

    Cleaned up file spacing, removed some cruf

--- app/models/project.rb
+++ app/models/project.rb
@@ -34,8 +34,7 @@ class Project < ActiveRecord::Base
   has_many :mhc_seqs
   has_many :genders
   belongs_to :owner, :class_name => "User", :foreign_key => "user_id"
-  # has_and_belongs_to_many :users
-  belongs_to  :user
+  belongs_to :user
 
   before_create :assign_project_owner
   after_save :assign_default_project
