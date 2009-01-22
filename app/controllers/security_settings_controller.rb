9e3e368f5d730a572bb38484eddd1738bc3430ae
james burrett <james@burrett.org>
2009-01-22 11:08:52 -0500

    security setting search, addition query map links

--- app/controllers/security_settings_controller.rb
+++ app/controllers/security_settings_controller.rb
@@ -3,22 +3,21 @@ class SecuritySettingsController < ApplicationController
   # version 6
   active_scaffold :security_settings do |config|
     config.columns = [:project, :user, :to_label, :level]
-
     config.columns[:project].form_ui = :select
-
     config.columns[:user].form_ui = :select
     config.columns[:to_label].form_ui = :select
-
     config.columns[:to_label].label = "Security Setting"
     config.columns[:to_label].sort_by :sql => 'level' 
-
     config.list.sorting = {:to_label => :asc}
-
     config.list.columns.exclude :level
     config.create.columns.exclude :to_label
     config.update.columns.exclude :to_label
+    config.columns[:user].search_sql = 'users.login'
+    config.search.columns << :user 
+    config.columns[:project].search_sql = 'projects.name'
+    config.search.columns << :project 
   end
-
+  
   def conditions_for_collection
     # Admins can see all users and if they don't have read
     # permissions, the projects table won't be included
@@ -27,7 +26,7 @@ class SecuritySettingsController < ApplicationController
       # If not, and admin, only show security settings
       # they manage
       ['projects.user_id = (?)', current_user.id]
-#    ['(projects.user_id = (?) OR EXISTS (SELECT 1 FROM security_settings where security_settings.project_id = projects.id AND security_settings.user_id = ? AND security_settings.level > 0))', current_user.id, current_user.id]
     end
   end
+
 end
