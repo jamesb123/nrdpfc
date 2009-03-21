47b1924eac16b15aefd7e4f683369fe21750b36e
james burrett <james@burrett.org>
2009-03-20 21:05:56 -0400

    fix spelling error

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -13,7 +13,8 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   include CurrentProjectHelper
   include InPlaceEditing
-  
+  include ExceptionNotifiable
+
   prepend_before_filter :login_required  
   
   ActiveScaffold.set_defaults do |config|
@@ -52,4 +53,22 @@ class ApplicationController < ActionController::Base
 
     send_data csv_string, :filename => "#{table_name}.csv"
   end
+
+  def client_browser_name 
+                          user_agent = request.env['HTTP_USER_AGENT'].downcase 
+                          if user_agent =~ /msie/i 
+                                  "Internet Explorer" 
+                          elsif user_agent =~ /konqueror/i 
+                                  "Konqueror" 
+                          elsif user_agent =~ /gecko/i 
+                                  "Mozilla" 
+                          elsif user_agent =~ /opera/i 
+                                  "Opera" 
+                          elsif user_agent =~ /applewebkit/i 
+                                  "Safari" 
+                          else 
+                                  "Unknown" 
+                          end 
+  end 
+
 end
