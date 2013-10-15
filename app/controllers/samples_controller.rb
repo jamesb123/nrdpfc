4a0e6687b420aab37ea03c692e4d7253b3c045f9
James Burrett <james@burrett.org>
2013-10-15 18:51:11 -0400

    remove variable columns, fix missing chicken columns

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -2,7 +2,7 @@ class SamplesController < ApplicationController
   layout "tabs"
   @@cnt=0
   before_filter :update_table_config
-  CHICKEN_INCLUDE = [:chicken_barcode, :chicken_sample_date, :chicken_contact, :chicken_company, :chicken_strain, :chicken_feathering, :chicken_package, :chicken_declared_gender, :chicken_meat_part, :chicken_ml_duplicate, :chicken_comments]
+  CHICKEN_INCLUDE = [ :chicken_company, :chicken_feathering, :chicken_declared_gender, :chicken_meat_part, :chicken_ml_duplicate]
   WOLF_EXCLUDE_LIST =   [:sample_bc, :project, :type_lat_long, :locality_type, :locality_type_text,  :security_settings, :approved, :date_submitted, :sample_id, :organism_id, :discrepancy_comments,:remote_data_entry, :id]
   WHALES_EXCLUDE_LIST = [:sample_bc, :project, :type_lat_long, :locality_type, :locality_type_text,  :security_settings, :approved, :remote_data_Entry, :platebc, :plateposition, :country,
   :province, :location_measurement_method, :location_1, :location_2, :location_accuracy,:age, :condition, :rehydrated, :diet_analysis ]
@@ -15,20 +15,20 @@ class SamplesController < ApplicationController
   
   SAMPLES_COLUMNS = [:id, :organism_id, :organism, :organism_index, :sample_bc, :field_code, :country, :province, 
     :locality, :locality_type_text, :locality_comments, 
-    :location_1, :location_2, :location_accuracy,  
+    :location_1, :location_2, :location_3, :location_4, :location_accuracy, :type_lat_long, :locality_type,  
     :latitude, :longitude, :coordinate_system,  :location_measurement_method,    
-    :collected_on_day,  :collected_on_month, :collected_on_year, :collected_by, :collector_comments, 
+    :collected_on_day,  :collected_on_month, :collected_on_year, :collected_by, :age, :condition, :rehydrated, :diet_analysis,
     :date_received, :received_by, :receiver_comments, :date_submitted, :submitted_by,  
-    :submitter_comments, :tissue_type, :extraction_method, :shippingmaterial,
+    :tissue_type, :text_tissue_type, :extraction_method, :shippingmaterial,
     :platebc, :plateposition, :batch_number, 
     :storage_medium, :storage_building, :storage_room, :storage_fridge, :storage_box,
     :xy_position, :tissue_remaining,  :security_settings,:approved, 
     :shipping_date, :organization, :field_ident, :current_location, :comments, :import_permit, :export_permit,
-    :profiling_completed,:profiling_done_by,:profiling_funded_by,:profile_published, :publication_name, :profiling_date, :photo_id, :discrepancy_comments, :sample_image1 ]
+    :profiling_completed,:profiling_done_by,:profiling_funded_by,:profile_published, :publication_name, :profiling_date, :photo_id, :discrepancy, :discrepancy_comments, :sample_image1, :submitter_comments, :collector_comments ]
 
   def update_table_config
-    active_scaffold_config.columns.exclude SAMPLES_COLUMNS 
-    active_scaffold_config.columns.add SAMPLES_COLUMNS 
+  #  active_scaffold_config.columns.exclude SAMPLES_COLUMNS 
+  #  active_scaffold_config.columns.add SAMPLES_COLUMNS 
 
     # variable labels
     @proj = Project.find(current_project_id)
@@ -37,30 +37,42 @@ class SamplesController < ApplicationController
     active_scaffold_config.columns[:organism].label = @proj.organism_label 
     
     # project based columns
-    if !@proj.opt_col_1.nil? && !@proj.opt_col_1.empty? 
-      active_scaffold_config.list.columns.add @proj.opt_col_1
-      active_scaffold_config.create.columns.add @proj.opt_col_1
-      active_scaffold_config.update.columns.add @proj.opt_col_1
-      active_scaffold_config.show.columns.add @proj.opt_col_1
-    end
-    if !@proj.opt_col_2.nil? && !@proj.opt_col_2.empty? 
-      active_scaffold_config.list.columns.add @proj.opt_col_2
-      active_scaffold_config.create.columns.add @proj.opt_col_2
-      active_scaffold_config.update.columns.add @proj.opt_col_2
-      active_scaffold_config.show.columns.add @proj.opt_col_2
-    end
-    if !@proj.opt_col_3.nil? && !@proj.opt_col_3.empty? 
-      active_scaffold_config.list.columns.add @proj.opt_col_3
-      active_scaffold_config.create.columns.add @proj.opt_col_3
-      active_scaffold_config.update.columns.add @proj.opt_col_3
-      active_scaffold_config.show.columns.add @proj.opt_col_3
-    end
-    if !@proj.opt_col_4.nil? && !@proj.opt_col_4.empty? 
-      active_scaffold_config.list.columns.add @proj.opt_col_4
-      active_scaffold_config.create.columns.add @proj.opt_col_4
-      active_scaffold_config.update.columns.add @proj.opt_col_4
-      active_scaffold_config.show.columns.add @proj.opt_col_4
-    end
+#    if !@proj.opt_col_1.nil? && !@proj.opt_col_1.empty? 
+#      active_scaffold_config.list.columns.add @proj.opt_col_1
+#      active_scaffold_config.create.columns.add @proj.opt_col_1
+#      active_scaffold_config.update.columns.add @proj.opt_col_1
+#      active_scaffold_config.show.columns.add @proj.opt_col_1
+#    end
+#    if !@proj.opt_col_2.nil? && !@proj.opt_col_2.empty? 
+#      active_scaffold_config.list.columns.add @proj.opt_col_2
+#      active_scaffold_config.create.columns.add @proj.opt_col_2
+#      active_scaffold_config.update.columns.add @proj.opt_col_2
+#      active_scaffold_config.show.columns.add @proj.opt_col_2
+#    end
+#    if !@proj.opt_col_3.nil? && !@proj.opt_col_3.empty? 
+#      active_scaffold_config.list.columns.add @proj.opt_col_3
+#      active_scaffold_config.create.columns.add @proj.opt_col_3
+#      active_scaffold_config.update.columns.add @proj.opt_col_3
+#      active_scaffold_config.show.columns.add @proj.opt_col_3
+#    end
+#    if !@proj.opt_col_4.nil? && !@proj.opt_col_4.empty? 
+#      active_scaffold_config.list.columns.add @proj.opt_col_4
+#      active_scaffold_config.create.columns.add @proj.opt_col_4
+#      active_scaffold_config.update.columns.add @proj.opt_col_4
+#     active_scaffold_config.show.columns.add @proj.opt_col_4
+#    end
+#    if !@proj.opt_col_5.nil? && !@proj.opt_col_5.empty? 
+#     active_scaffold_config.list.columns.add @proj.opt_col_5
+#      active_scaffold_config.create.columns.add @proj.opt_col_5
+#      active_scaffold_config.update.columns.add @proj.opt_col_5
+#      active_scaffold_config.show.columns.add @proj.opt_col_5
+#    end
+#    if !@proj.opt_col_6.nil? && !@proj.opt_col_6.empty? 
+#      active_scaffold_config.list.columns.add @proj.opt_col_6
+#      active_scaffold_config.create.columns.add @proj.opt_col_6
+#      active_scaffold_config.update.columns.add @proj.opt_col_6
+#      active_scaffold_config.show.columns.add @proj.opt_col_6
+#    end
 
     # columns for road runner / chickens
     if current_project_id == 66
@@ -68,13 +80,20 @@ class SamplesController < ApplicationController
         active_scaffold_config.create.columns.add CHICKEN_INCLUDE
         active_scaffold_config.update.columns.add CHICKEN_INCLUDE
         active_scaffold_config.show.columns.add CHICKEN_INCLUDE
+        active_scaffold_config.columns[:submitter_comments].label = "Strain"
+        active_scaffold_config.columns[:collector_comments].label = "Package Role"
+        active_scaffold_config.columns[:collected_by].label = "Contact Person"
+ 
     else
         active_scaffold_config.list.columns.exclude CHICKEN_INCLUDE
         active_scaffold_config.create.columns.exclude CHICKEN_INCLUDE
         active_scaffold_config.update.columns.exclude CHICKEN_INCLUDE
         active_scaffold_config.show.columns.exclude CHICKEN_INCLUDE
+        active_scaffold_config.columns[:submitter_comments].label = "Submitter Comments"
+        active_scaffold_config.columns[:collector_comments].label = "Collector Comments"
+        active_scaffold_config.columns[:collected_by].label = "Collected By"
     end
-  end
+end
 
   def org_update_table_config
     active_scaffold_config.list.columns.add  SAMPLES_COLUMNS
@@ -135,7 +154,8 @@ class SamplesController < ApplicationController
     config.nested.add_link("MS", [:microsatellites])
     config.nested.add_link("MHC", [:mhcs])
     config.nested.add_link("Y", [:y_chromosomes])
-    
+ 
+ 
     config.columns[:id].label = "Sample ID"
     config.columns[:project].label = "project Code"
     config.columns[:organism_id].label = "Organism ID"
@@ -176,7 +196,6 @@ class SamplesController < ApplicationController
     config.columns[:receiver_comments].label = "Receiver Comments"
     config.columns[:date_submitted].label = "Date Submitted"
     config.columns[:submitted_by].label = "Submitted by"
-    config.columns[:submitter_comments].label = "Submitter Comments"
     config.columns[:latitude].label = "Latitude"
     config.columns[:longitude].label = "Longitude"
     config.columns[:coordinate_system].label = "Coordinate System"
